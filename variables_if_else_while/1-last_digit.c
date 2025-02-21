f716332a185aab0dc1a41036b1f50ccad3a77acd
GsNrio <tonedstir@gmail.com>
2025-02-21 09:21:32 -0400

    test

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -16,7 +16,7 @@ int main(void)
 	{
 		printf("Last digit of n is %d and is greater than 5\n", n);
 	}
-	else if (n <6)
+	else if (n < 6)
 	{
 		printf("Last digit of n is %d and is less than 6 and not 0\n", n);
 	}
