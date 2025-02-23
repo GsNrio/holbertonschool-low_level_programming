58a4d069b15be30c1d503495a0ef9c1ce54cd5e9
GsNrio <tonedstir@gmail.com>
2025-02-23 13:16:26 -0400

    This program prints all the hexadecimals with lowercase format followed by a new line

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+/**
+ * main - Entry point of this program
+ *
+ * Return: 0
+ *
+ * This program prints out
+ * all the hexadecimals
+ * while employing letters
+ * to the task only on lowercase
+ * and the sequence is
+ * followed by a new line
+ */
+int main(void)
+{
+	int i;
+	char b;
+
+	for (i = 0; i <= 9; i++)
+	{
+		putchar(i);
+	}
+	for (b = 'a'; b <= 'f'; b++)
+	{
+		putchar(b);
+		putchar('\n');
+	}
+	return (0);
+}
