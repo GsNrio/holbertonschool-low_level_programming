4acf8c12ed2a238060deced49f14e7bca3cc53b5
GsNrio <tonedstir@gmail.com>
2025-02-23 20:31:19 -0400

    This program prints the text putchar in a given format

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+/**
+ * main - Entry point of the program
+ *
+ * Return: always 0
+ *
+ * This program prints
+ * the text putchar
+ * in a specified format
+ * followed by a new line
+ */
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
