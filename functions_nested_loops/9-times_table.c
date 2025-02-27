d5944941daab9f6227bca66c1c04f1c443709566
GsNrio <tonedstir@gmail.com>
2025-02-27 10:24:24 -0400

    This function prints the 9 times table

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -16,23 +16,24 @@ void times_table(void)
 		for (j = 0; j <= 9; j++)
 		{
 			result = i * j;
-
-			if (j > 0)
-				_putchar(' ');
-
-			if (result < 10)
-				_putchar(result + '0');
-			else
+			if (result > 9) 
 			{
 				_putchar(result / 10 + '0');
 				_putchar(result % 10 + '0');
 			}
-
-			if (j < 9)
+			else if(j != 0)
+			{
+				_putchar(' ');
+				_putchar(result + '0');
+			}
+			else
+				_putchar(result + '0');
+			if (j != 9)
 			{
 				_putchar(',');
 				_putchar(' ');
 			}
+					
 		}
 		_putchar('\n');
 	}
