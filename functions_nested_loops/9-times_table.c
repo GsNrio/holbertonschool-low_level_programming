2c484c9f94958e14507ac475b70295d7f18a822c
GsNrio <tonedstir@gmail.com>
2025-02-27 01:59:41 -0400

    This function prints a 9 times table

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -5,29 +5,35 @@
  * @void: no return.
  *
  * Return: void
-*/
+ */
 void times_table(void)
 {
 
-	int i = 0;
+	int i, j, result;
 
-	while (i <= 9)
-		i++;
-
+	for (i = 0; i <= 9; i++)
 	{
-
-		while (i <= 9)
-
+		for (j = 0; j <= 9; j++)
 		{
+			result = i * j;
 
-		_putchar(i);
-		_putchar(',');
-		_putchar(' ');
+			if (j > 0)
+				_putchar(' ');
 
+			if (result < 10)
+				_putchar(result + '0');
+			else
+			{
+				_putchar(result / 10 + '0');
+				_putchar(result % 10 + '0');
+			}
+
+			if (j < 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
 		}
-
+		_putchar('\n');
 	}
-
-	_putchar ('\n');
-
 }
