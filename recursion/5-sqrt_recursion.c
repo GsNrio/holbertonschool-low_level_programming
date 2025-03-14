bef019d2f25c6518d5030ed0e8194774e36bd009
GsNrio <tonedstir@gmail.com>
2025-03-14 19:10:10 -0400

    This function returns the natural square root of a number

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -1,32 +1,15 @@
 #include "main.h"
 #include <stdio.h>
-#include <math.h>
-
-int actual_sqrt_recursion(int n, int s)
 
 /**
- * _sqrt_recursion - returns the natural square root of a number.
- * @n: an integer.
+ * actual_sqrt_recursion - Recurses to find
+ * the natural square root of a number.
+ * @n: The number to find the square root of.
+ * @s: The current guess for the square root.
  *
- * Return: the resulting square root.
-*/
-int _sqrt_recursion(int n);
-{
-	if (n < 0)
-		return (-1);
-	return (actual_sqrt_recursion(n, 0));
-}
-
- /**
- * actual _sqrt_recursion - recurses to find the natural
- * aquare root of a number
- *
- * @n: an the number to which find the square root.
- * @s: the current guess for the square root.
- *
- *Return: the resulting square root.
-*/
-
+ * Return: The resulting square root, or -1 if
+ * no square root is found.
+ */
 int actual_sqrt_recursion(int n, int s)
 {
 	if (s * s == n)
@@ -34,4 +17,19 @@ int actual_sqrt_recursion(int n, int s)
 	if (s * s > n)
 		return (-1);
 	return (actual_sqrt_recursion(n, s + 1));
+}
+
+/**
+ * _sqrt_recursion - Returns the natural
+ * square root of a number.
+ * @n: The integer to find the square root of.
+ *
+ * Return: The resulting square root, or -1 if
+ * n is negative.
+ */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	return (actual_sqrt_recursion(n, 0));
 }
