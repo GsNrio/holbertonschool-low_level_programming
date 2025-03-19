ca7a233340371a419b353f6581e58b652922b2a9
GsNrio <tonedstir@gmail.com>
2025-03-19 19:00:48 -0400

    This program multiplies two numbers

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
+ * main - entry point to the program.
  *
+ * @argc: integer.
  *
+ * @argv: pointer to an array of string.
  *
+ * Return: 1 if the program doesn't receive two arguments; otherwise 0.
  *
- *
- *
- *
- */
+*/
 int main(int argc, char *argv[])
 {
 	int a;
