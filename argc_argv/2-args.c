1257ccc4b7608cfb95dcdd3c9522e0568cfbf299
GsNrio <tonedstir@gmail.com>
2025-03-19 15:51:52 -0400

    This program prints all argumentsit receives

--- argc_argv/2-args.c
+++ argc_argv/2-args.c
@@ -1,14 +1,22 @@
 #include <stdlib.h>
 #include <stdio.h>
 /**
- * main entry - point to the program.
+ * main - entry point to the program.
  *
- * @argc: number of argumentsin the program.
+ * @argc: number of arguments in the program.
  *
  * @argv: array of arguments in the program.
  *
  * Return: nothing.
-*/
-int main(int argc, char *argv[])
+ */
+int main(int __attribute__((__unused__)) argc, char *argv[])
 {
+	int i;
 
+	i = 0;
+	for (; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
